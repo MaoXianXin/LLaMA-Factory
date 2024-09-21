f6373788ee64a199471f84f84209ccfb68e76c2e
MaoXianxin <My1044467857@yahoo.com>
2024-09-21 18:17:41 +0800

    qwen2 lora merge

--- examples/merge_lora/merge.sh
+++ examples/merge_lora/merge.sh
@@ -2,10 +2,10 @@
 # DO NOT use quantized model or quantization_bit when merging lora weights
 
 CUDA_VISIBLE_DEVICES=0 python ../../src/export_model.py \
-    --model_name_or_path meta-llama/Llama-2-7b-hf \
-    --adapter_name_or_path ../../saves/LLaMA2-7B/lora/sft \
-    --template default \
+    --model_name_or_path /home/mao/github_repo/LLaMA-Factory/models--Qwen--Qwen2-0.5B-Instruct/snapshots/c291d6fce4804a1d39305f388dd32897d1f7acc4 \
+    --adapter_name_or_path ../../saves/Qwen2-0.5B-Instruct/lora/train_2024-09-19 \
+    --template qwen \
     --finetuning_type lora \
-    --export_dir ../../models/llama2-7b-sft \
+    --export_dir ../../models/Qwen2-0.5B-sft \
     --export_size 2 \
     --export_legacy_format False
